6ca2df3a7dd96fb1154995f2988aa1f14c80db40
Jason Walsh <j@wal.sh>
2015-03-17 23:50:51 -0400

    Reducing tests for the MIT profile model
    Tests are still expected to fail but have been reduced to conform to
    the data we're expecting to see from the strategy.

--- spec/omniauth/strategies/mit_spec.rb
+++ spec/omniauth/strategies/mit_spec.rb
@@ -30,57 +30,17 @@ describe OmniAuth::Strategies::MIT do
 
   context "client options" do
     it 'should have correct site' do
-      subject.options.client_options.site.should eq("https://api.github.com")
+      subject.options.client_options.site.should eq("https://oidc.mit.edu/manage/dev/dynreg")
     end
 
     it 'should have correct authorize url' do
-      subject.options.client_options.authorize_url.should eq('https://github.com/login/oauth/authorize')
+      subject.options.client_options.authorize_url.should eq(' https://oidc.mit.edu/authorize')
     end
 
     it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq('https://github.com/login/oauth/access_token')
+      subject.options.client_options.token_url.should eq('https://oidc.mit.edu/token')
     end
 
-    describe "should be overrideable" do
-      it "for site" do
-        enterprise.options.client_options.site.should eq(enterprise_site)
-      end
-
-      it "for authorize url" do
-        enterprise.options.client_options.authorize_url.should eq(enterprise_authorize_url)
-      end
-
-      it "for token url" do
-        enterprise.options.client_options.token_url.should eq(enterprise_token_url)
-      end
-    end
-  end
-
-  context "#email_access_allowed?" do
-    it "should not allow email if scope is nil" do
-      subject.options['scope'].should be_nil
-      subject.should_not be_email_access_allowed
-    end
-
-    it "should allow email if scope is user" do
-      subject.options['scope'] = 'user'
-      subject.should be_email_access_allowed
-    end
-
-    it "should allow email if scope is a bunch of stuff including user" do
-      subject.options['scope'] = 'public_repo,user,repo,delete_repo,gist'
-      subject.should be_email_access_allowed
-    end
-
-    it "should not allow email if scope does not grant email access" do
-      subject.options['scope'] = 'repo,user:follow'
-      subject.should_not be_email_access_allowed
-    end
-
-    it "should assume email access not allowed if scope is something currently not documented " do
-      subject.options['scope'] = 'currently_not_documented'
-      subject.should_not be_email_access_allowed
-    end
   end
 
   context "#email" do
@@ -94,49 +54,6 @@ describe OmniAuth::Strategies::MIT do
       subject.email.should be_nil
     end
 
-    it "should return the primary email if there is no raw_info and email access is allowed" do
-      emails = [
-        { 'email' => 'secondary@example.com', 'primary' => false },
-        { 'email' => 'primary@example.com',   'primary' => true }
-      ]
-      subject.stub!(:raw_info).and_return({})
-      subject.options['scope'] = 'user'
-      subject.stub!(:emails).and_return(emails)
-      subject.email.should eq('primary@example.com')
-    end
-
-    it "should return the first email if there is no raw_info and email access is allowed" do
-      emails = [
-        { 'email' => 'first@example.com',   'primary' => false },
-        { 'email' => 'second@example.com',  'primary' => false }
-      ]
-      subject.stub!(:raw_info).and_return({})
-      subject.options['scope'] = 'user'
-      subject.stub!(:emails).and_return(emails)
-      subject.email.should eq('first@example.com')
-    end
-  end
-
-  context "#raw_info" do
-    it "should use relative paths" do
-      access_token.should_receive(:get).with('user').and_return(response)
-      subject.raw_info.should eq(parsed_response)
-    end
-  end
-
-  context "#emails" do
-    it "should use relative paths" do
-      access_token.should_receive(:get).with('user/emails', :headers=>{"Accept"=>"application/vnd.github.v3"}).and_return(response)
-      subject.options['scope'] = 'user'
-      subject.emails.should eq(parsed_response)
-    end
-  end
-
-  context '#info.urls' do
-    it 'should use html_url from raw_info' do
-      subject.stub(:raw_info).and_return({ 'login' => 'me', 'html_url' => 'http://enterprise/me' })
-      subject.info['urls']['MIT'].should == 'http://enterprise/me'
-    end
   end
 
 end
